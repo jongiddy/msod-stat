79ee3dbbdf1c24d559919e799c8265dd96b37d8d
Jonathan Giddy <jongiddy@gmail.com>
2018-08-26 16:46:20 +0100

    Working authorization code grant using client secret

---
+++ src/auth.rs
@@ -0,0 +1,133 @@
+use std::io;
+use oauth2::prelude::*;
+use oauth2::{
+    AuthorizationCode,
+    AuthType,
+    AuthUrl,
+    ClientId,
+    ClientSecret,
+    CsrfToken,
+    RedirectUrl,
+    Scope,
+    TokenUrl
+};
+use oauth2::basic::{BasicClient, BasicTokenResponse, BasicRequestTokenError};
+use open;
+use tiny_http::{Server, Response, Method, StatusCode};
+use url::Url;
+
+fn extract_authorization_code(url: &Url, csrf_token: &CsrfToken) -> Option<String> {
+    // Looking for
+    // /redirect?code=Mac..dc6&state=DL7jz5YIW4WusaYdDZrXzA%3d%3d
+    let mut received_code = None;
+    let mut received_state = None;
+    for pair in url.query_pairs() {
+        match pair.0.as_ref() {
+            "code" => {
+                if received_code.is_some() {
+                    println!("Duplicate code");
+                    return None;
+                }
+                received_code = Some(pair.1.into_owned());
+            },
+            "state" => {
+                if received_state.is_some() {
+                    println!("Duplicate state");
+                    return None;
+                }
+                received_state = Some(pair.1);
+            },
+            parameter => {
+                println!("Unexpected parameter: {}", parameter);
+                return None;
+            }
+        }
+    }
+    if received_state.as_ref().unwrap() != csrf_token.secret() {
+        println!("CSRF token mismatch");
+        return None;
+    }
+    if received_code.is_none() {
+        println!("No authorization code received");
+    }
+    return received_code;
+}
+
+fn get_authorization_code(
+    auth_url: Url,
+    csrf_token: CsrfToken,
+) -> Result<String, io::Error> {
+
+    let server = Server::http("localhost:3003").unwrap();
+
+    if let Err(e) = open::that(auth_url.as_str()) {
+        println!("{}", e);
+        println!("Browse to {}", auth_url);
+    }
+
+    for request in server.incoming_requests() {
+        let status_code = match request.method() {
+            Method::Get => {
+                let base = Url::parse("http://localhost:3003/").unwrap();
+                let url = base.join(request.url()).unwrap();
+                if url.path() == "/redirect" {
+                    match extract_authorization_code(&url, &csrf_token) {
+                        None => StatusCode(404),
+                        Some(code) => {
+                            let response = Response::from_string("You may now close this window.");
+                            request.respond(response)?;
+                            return Ok(code)
+                        }
+                    }
+                }
+                else {
+                    println!("Unrecognized path: {}", request.url());
+                    StatusCode(404)
+                }
+            },
+            _ => {
+                println!("Unsupported method: {}", request.method());
+                StatusCode(404)
+            }
+        };
+
+        let response = Response::from_string(status_code.default_reason_phrase())
+            .with_status_code(status_code);
+        request.respond(response)?;
+    }
+
+    panic!("No more incoming connections and auth code not supplied")
+}
+
+pub fn authenticate(client_id: String, client_secret: String)
+    -> Result<BasicTokenResponse, BasicRequestTokenError>
+{
+    let ms_graph_authorize_url = AuthUrl::new(
+        Url::parse("https://login.microsoftonline.com/common/oauth2/v2.0/authorize").unwrap()
+    );
+    let ms_graph_token_url = Some(
+        TokenUrl::new(
+            Url::parse("https://login.microsoftonline.com/common/oauth2/v2.0/token").unwrap()
+        )
+    );
+
+    let client =
+        BasicClient::new(
+            ClientId::new(client_id),
+            Some(ClientSecret::new(client_secret)),
+            ms_graph_authorize_url,
+            ms_graph_token_url
+        )
+        .set_auth_type(AuthType::RequestBody)
+        .add_scope(Scope::new("Files.Read.All".to_string()))
+        .set_redirect_url(
+            RedirectUrl::new(Url::parse("http://localhost:3003/redirect").unwrap())
+        );
+
+    // Generate the full authorization URL.
+    let (auth_url, csrf_token) = client.authorize_url(CsrfToken::new_random);
+
+    let authorization_code = get_authorization_code(auth_url, csrf_token).unwrap();
+
+    client.exchange_code(AuthorizationCode::new(authorization_code))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+extern crate open;
+extern crate oauth2;
+extern crate reqwest;
+extern crate tiny_http;
+extern crate url;
+
+use std::io::{self, BufRead};
+use reqwest::{header, StatusCode};
+use oauth2::prelude::*;
+use oauth2::basic::BasicTokenType;
+
+mod auth;
+
+
 fn main() {
-    println!("Hello, world!");
+    // See https://docs.microsoft.com/en-us/onedrive/developer/rest-api/getting-started/graph-oauth
+    // To get a username/password for an app:
+    // 1. Go to https://apps.dev.microsoft.com/
+    // 2. Click Add an App.
+    // 3. Skip the guided setup.
+    // 4. Set Web Redirect URL to http://localhost:3000/redirect
+    // 5. Add Delegated Permissions of Files.Read.All
+    // 6. Copy the Application Id as the username.
+    // 7. Click Generate New Password.
+    // 8. Copy the password.
+    // 9. Create a credentials file containing the username and password on separate lines.
+    // 10. Pipe the credentials file into this command.
+
+    let mut username = String::new();
+    let mut password = String::new();
+    let stdin = io::stdin();
+    {
+        let mut buf = stdin.lock();
+        buf.read_line(&mut username).unwrap();
+        buf.read_line(&mut password).unwrap();
+        username.pop();
+        password.pop();
+    }
+    let token = auth::authenticate(username, password).unwrap();
+    println!("{:?}", token);
+    let mut headers = header::Headers::new();
+    match token.token_type() {
+        BasicTokenType::Bearer => {
+            headers.set(
+                header::Authorization(
+                    header::Bearer {
+                       token: token.access_token().secret().to_string()
+                   }
+               )
+            );
+        },
+        BasicTokenType::Mac => {
+            panic!("reqwest does not support MAC Authorization")
+        }
+    }
+
+    let client = reqwest::Client::builder().default_headers(headers).build().unwrap();
+
+    let mut response = client.get("https://graph.microsoft.com/v1.0/me/drives").send().unwrap();
+    if response.status() != StatusCode::Ok {
+        panic!("{:?} {}", response.status(), response.status().canonical_reason().unwrap());
+    }
+    let result = response.text().unwrap();
+    println!("{:?}", response);
+    println!("{}", result);
 }
