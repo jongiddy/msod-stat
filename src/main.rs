cc9984df3242920a0dd9323b18ffdb7c4c443b32
Jonathan Giddy <jongiddy@gmail.com>
2019-01-02 01:24:08 +0000

    Use OneDrive Sync for faster retrieval

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,131 @@ extern crate url;
 
 use std::io::{self, BufRead, Write};
 use reqwest::{header, StatusCode};
-use serde_json::Value;
+use serde_json::{json, Value};
 use oauth2::prelude::*;
 use oauth2::basic::BasicTokenType;
 
 mod auth;
 
 
-fn process_drive(client: &reqwest::Client, drive_id: &str) -> (u32, u32) {
-    let uri = format!("https://graph.microsoft.com/v1.0/me/drives/{}/root/children", drive_id);
-    process_items(client, drive_id, &uri)
+struct DriveSyncPageIterator<'a> {
+    client: &'a reqwest::Client,
+    next_link: Option<String>,
 }
 
-fn process_items(client: &reqwest::Client, drive_id: &str, uri: &str) -> (u32, u32) {
-    let mut file_count = 0;
-    let mut folder_count = 0;
-    let mut response = client.get(uri).send().unwrap();
+impl<'a> DriveSyncPageIterator<'a> {
+    fn new<'b>(client: &'b reqwest::Client, drive_id: &str) -> DriveSyncPageIterator<'b> {
+        let link = format!("https://graph.microsoft.com/v1.0/me/drives/{}/root/delta", drive_id);
+        DriveSyncPageIterator {
+            client: client,
+            next_link: Some(link),
+        }
+    }
+}
+
+fn get(client: &reqwest::Client, uri: &str) -> Result<String, reqwest::Error> {
+    let mut response = client.get(uri).send()?;
     if response.status() != StatusCode::OK {
         panic!("{:?} {}", response.status(), response.status().canonical_reason().unwrap());
     }
-    let result = response.text().unwrap();
-    let json: Value = serde_json::from_str(&result).unwrap();
-    for item in json["value"].as_array().unwrap() {
+    response.text()
+}
+
+impl<'a> Iterator for DriveSyncPageIterator<'a> {
+    type Item = Value;
+
+    fn next(&mut self) -> Option<Value> {
+        let next_link = std::mem::replace(&mut self.next_link, None);
+        match next_link {
+            None => {
+                None
+            },
+            Some(uri) => {
+                let mut json: Value = match get(self.client, &uri) {
+                    Ok(result) => {
+                        serde_json::from_str(&result).unwrap()
+                    },
+                    Err(err) => {
+                        println!("Retrying: {:?}", err);
+                        let result = get(self.client, &uri).unwrap();
+                        serde_json::from_str(&result).unwrap()
+                    }
+                };
+                self.next_link = json.get("@odata.nextLink").map(|v| v.as_str().unwrap().to_owned());
+                json.get_mut("value").map(Value::take)
+            }
+        }
+    }
+}
+
+struct DriveSyncItemIterator<'a> {
+    page_iter: DriveSyncPageIterator<'a>,
+    items: Value,
+    item_index: usize,
+}
+
+impl<'a> DriveSyncItemIterator<'a> {
+    fn new<'b>(client: &'b reqwest::Client, drive_id: &str) -> DriveSyncItemIterator<'b> {
+        DriveSyncItemIterator {
+            page_iter: DriveSyncPageIterator::new(client, drive_id),
+            items: json!([]),
+            item_index: 1,
+        }
+    }
+}
+
+impl<'a> Iterator for DriveSyncItemIterator<'a> {
+    type Item = Value;
+
+    fn next(&mut self) -> Option<Value> {
+        match self.item_index {
+            0 => None,
+            _ => {
+                if self.item_index >= self.items.as_array().unwrap().len() {
+                    let value = self.page_iter.next();
+                    match value {
+                        None => {
+                            None
+                        },
+                        Some(items) => {
+                            self.items = items;
+                            self.item_index = 1;
+                            self.items.get_mut(0).map(Value::take)
+                        }
+                    }
+                }
+                else {
+                    let val = self.items.get_mut(self.item_index).map(Value::take);
+                    self.item_index += 1;
+                    val
+                }
+            }
+        }
+    }
+}
+
+fn process_drive(client: &reqwest::Client, drive_id: &str) -> (u32, u32) {
+    let mut seen = std::collections::HashSet::<String>::new();
+    let mut file_count = 0;
+    let mut folder_count = 0;
+    for mut item in DriveSyncItemIterator::new(client, drive_id) {
+        if item.get("deleted").is_some() {
+            println!("{:?}", item);
+            continue;
+        }
+        let mut id_value = item.get_mut("id").map(Value::take).unwrap();
+        let id = id_value.as_str().unwrap();
+        if seen.contains(id) {
+            continue;
+        }
+        seen.insert(id.to_owned());
         if item.get("file").is_some() {
             file_count += 1;
         }
-        else if item.get("folder").is_some() || item.get("specialFolder").is_some() {
+        else if item.get("folder").is_some() || item.get("package").is_some() {
             folder_count += 1;
-            let item_id = item["id"].as_str().unwrap();
-            let uri = format!("https://graph.microsoft.com/v1.0/me/drives/{}/items/{}/children", drive_id, item_id);
             print!(".");
             io::stdout().flush().unwrap();
-            let (file_inc, folder_inc) = process_items(client, drive_id, &uri);
-            file_count += file_inc;
-            folder_count += folder_inc;
         }
         else {
             print!("(ignoring {})", item["name"].as_str().unwrap());
@@ -91,7 +182,7 @@ fn main() {
         }
     }
 
-    let client = reqwest::Client::builder().default_headers(headers).build().unwrap();
+    let client = reqwest::Client::builder().timeout(None).default_headers(headers).build().unwrap();
 
     let mut response = client.get("https://graph.microsoft.com/v1.0/me/drives").send().unwrap();
     if response.status() != StatusCode::OK {
