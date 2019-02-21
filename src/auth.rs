73ff1ef1cca0f8982b4c1505e031a9ded5e9a343
Jonathan Giddy <jongiddy@gmail.com>
2019-02-21 19:40:51 +0000

    Select a random port to prevent conflicts for redirect URL

--- src/auth.rs
+++ src/auth.rs
@@ -1,4 +1,7 @@
+use std::error::Error;
 use std::io;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use string_error;
 use oauth2::prelude::*;
 use oauth2::{
     AuthorizationCode,
@@ -15,6 +18,7 @@ use oauth2::{
 };
 use oauth2::basic::{BasicClient, BasicTokenResponse, BasicRequestTokenError};
 use open;
+use rand::{thread_rng, Rng};
 use tiny_http::{Server, Response, Method, StatusCode};
 use url::Url;
 
@@ -56,16 +60,9 @@ fn extract_authorization_code(url: &Url, csrf_token: &CsrfToken) -> Option<Strin
 }
 
 fn get_authorization_code(
-    auth_url: Url,
+    server: &Server,
     csrf_token: CsrfToken,
 ) -> Result<String, io::Error> {
-
-    let server = Server::http("localhost:3003").unwrap();
-
-    if let Err(e) = open::that(auth_url.as_str()) {
-        println!("{}", e);
-        println!("Browse to {}", auth_url);
-    }
 
     for request in server.incoming_requests() {
         let status_code = match request.method() {
@@ -101,6 +98,40 @@ fn get_authorization_code(
     panic!("No more incoming connections and auth code not supplied")
 }
 
+fn start_server() -> Result<Server, Box<dyn Error>> {
+    // MS Graph requires an exact match for the redirect URL. To reduce the chance of failing
+    // if a fixed port is in use, we try 8 different ports. The MS app must have 8 registered
+    // Redirect URI's: http://localhost:<port>/redirect for each value of <port>
+    let mut ports: [u16; 8] = [3003, 17465, 22496, 23620, 25243, 27194, 28207, 32483];
+    // Select ports in random order to prevent herding and add a bit of security through
+    // non-deterministic behavior.
+    let mut rng = thread_rng();
+    rng.shuffle(&mut ports);
+    let mut socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0);
+    for port in &ports {
+        socket.set_port(*port);
+        match Server::http(socket) {
+            Ok(server) => return Ok(server),
+            Err(err) => {
+                match err.downcast::<io::Error>() {
+                    Ok(io_err) => {
+                        if io_err.kind() == io::ErrorKind::AddrInUse {
+                            // try next port
+                        }
+                        else {
+                            return Err(io_err);
+                        }
+                    }
+                    Err(err) => {
+                        return Err(err);
+                    }
+                }
+            }
+        }
+    }
+    Err(string_error::static_err("Could not find an available port"))
+}
+
 pub fn authenticate(client_id: String, client_secret: String)
     -> Result<BasicTokenResponse, BasicRequestTokenError>
 {
@@ -113,6 +144,9 @@ pub fn authenticate(client_id: String, client_secret: String)
         )
     );
 
+    let server = start_server().unwrap();
+    let redirect_url = format!("http://localhost:{}/redirect", server.server_addr().port());
+
     let client =
         BasicClient::new(
             ClientId::new(client_id),
@@ -122,9 +156,7 @@ pub fn authenticate(client_id: String, client_secret: String)
         )
         .set_auth_type(AuthType::RequestBody)
         .add_scope(Scope::new("Files.Read.All".to_string()))
-        .set_redirect_url(
-            RedirectUrl::new(Url::parse("http://localhost:3003/redirect").unwrap())
-        );
+        .set_redirect_url(RedirectUrl::new(Url::parse(&redirect_url).unwrap()));
 
     // Setup PKCE code challenge
     let code_verifier = PkceCodeVerifierS256::new_random();
@@ -136,7 +168,15 @@ pub fn authenticate(client_id: String, client_secret: String)
         &code_verifier.authorize_url_params(),
     );
 
-    let authorization_code = get_authorization_code(auth_url, csrf_token).unwrap();
+    if let Err(e) = open::that(auth_url.as_str()) {
+        println!("{}", e);
+        println!("Browse to {}", auth_url);
+    }
+
+    let authorization_code = get_authorization_code(&server, csrf_token).unwrap();
+
+    // close down server
+    drop(server);
 
     // Send the PKCE code verifier in the token request
     let params: Vec<(&str, &str)> = vec![("code_verifier", &code_verifier.secret())];
